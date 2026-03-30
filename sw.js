afb03fac5fb3591c1f4434d31e5df0e27a28ee0f
ayounkworks <ayounk.chikal@gmail.com>
2026-03-30 14:01:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'comic-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (event) => {
-  self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  // versi simpel, belum pakai cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
